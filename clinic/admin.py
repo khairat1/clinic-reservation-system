2542444a47fe7dd6ed9f6cff82f8c6a14766070a
m4xn <78203994+m4xn@users.noreply.github.com>
2026-04-22 23:40:35 +0300

    Add Department, Doctor, Schedule models and register in admin

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Department, Doctor, Schedule
 
-# Register your models here.
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'department']
+
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ['doctor', 'day', 'start_time', 'end_time']
